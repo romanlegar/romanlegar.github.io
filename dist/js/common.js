d1f7809dfdb5586d936d7853153ceaf21a2f0b89
romanlegar <romalehar@gmail.com>
2018-10-13 02:14:49 +0300

    1.0

--- dist/js/common.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $(document).on('mousemove', function (e) {
-        $('.first').css({
-            left: -e.pageX / 10 + 200,
-            top: -e.pageY / 10 + 200
-        });
-    });
-});
